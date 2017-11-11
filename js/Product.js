6ec3ffbff189e07e807d4c29ff6551cbe71b2312
CharlyWelch <blwbiology@gmail.com>
2017-11-11 11:14:57 -0800

    add unsaved changes from prior commit

--- js/Product.js
+++ js/Product.js
@@ -17,39 +17,6 @@ class Product { //eslint-disable-line
         ele.name = this.name;
         return ele;
     }
-    renderTable() {
-        const tab = document.getElementById('tab');
-        const headRow = document.createElement('tr');
-        tab.appendChild(headRow);
-        const headBlank = document.createElement('th');
-        headRow.appendChild(headBlank);
-        const tabDisplay = document.createElement('th');
-        tabDisplay.textContent = 'Times Displayed';
-        headRow.appendChild(tabDisplay);
-        const tabClick = document.createElement('th');
-        tabClick.textContent = 'Times Clicked';
-        headRow.appendChild(tabClick);
-        const tabPercent = document.createElement('th');
-        tabPercent.textContent = 'Percentage Chosen';
-        headRow.appendChild(tabPercent);
-        for ( let i = 0; i < products.length; i++){
-            const productRow = document.createElement('tr');
-            const nameCell = document.createElement('th');
-            nameCell.textContent = this.name;
-            productRow.appendChild(nameCell);
-            const dispCell = document.createElement('td');
-            dispCell.textContent = this.displayed;
-            productRow.appendChild(dispCell);
-            const clickCell = document.createElement('td');
-            clickCell.textContent = this.clicked;
-            productRow.appendChild(clickCell);
-            const percentCell = document.createElement('td');
-            clickCell.textContent = (this.clicked / this.displayed) * 100;
-            productRow.appendChild(percentCell);
-            tab.appendChild(productRow);
-        }
-    }
-
 }
 
 
